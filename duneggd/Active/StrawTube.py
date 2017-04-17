b0284599e1f494841bffb674c9ab279033e4fa0b
palominogallo <jose.palominogallo@stonybrook.edu>
2017-04-17 17:34:49 -0400

    ckecking id version

--- duneggd/Active/StrawTube.py
+++ duneggd/Active/StrawTube.py
@@ -33,21 +33,22 @@ class StrawTubeBuilder(gegede.builder.Builder):
         Construct the geometry for Straw Tube Bar.
         :returns: None
         """
-        main_shape = geom.shapes.Tubs(self.name, rmin = self.halfDimension["rmin"],
-                                      rmax = self.halfDimension["rmax"], dz = self.halfDimension["dz"])
-        main_lv   = geom.structure.Volume(self.name+"_lv", material=self.Material, shape=main_shape)
+        wire_shape = geom.shapes.Tubs(self.name+"_wire", self.halfWireDimension["rmin"],
+                                              rmax = self.halfSTDimension["rmax"], dz = self.halfSTDimension["dz"])
+        wire_lv    = geom.structure.Volume(self.name+"_wire_lv", material=self.WireMaterial, shape=wire_shape)
+        #self.add_volume(wire_lv)
 
         straw_shape = geom.shapes.Tubs(self.name+"_straw", rmin = self.halfSTDimension["rmin"],
                                       rmax = self.halfSTDimension["rmax"], dz = self.halfSTDimension["dz"])
         straw_lv   = geom.structure.Volume(self.name+"_straw_lv", material=self.STMaterial, shape=straw_shape)
+        #self.add_volume(straw_lv)
 
-        wire_shape = geom.shapes.Tubs(self.name+"_wire", self.halfWireDimension["rmin"],
-                                      rmax = self.halfSTDimension["rmax"], dz = self.halfSTDimension["dz"])
-        wire_lv    = geom.structure.Volume('volWire_'+self.name, material=self.WireMaterial, shape=wire_shape)
+        main_shape = geom.shapes.Tubs(self.name, rmin = self.halfDimension["rmin"],
+                                      rmax = self.halfDimension["rmax"], dz = self.halfDimension["dz"])
+        main_lv   = geom.structure.Volume(self.name+"_lv", material=self.Material, shape=main_shape)
+        self.add_volume(main_lv)
 
         pS_in_Tube = geom.structure.Placement( 'placeS_in_Tube_'+self.name, volume = straw_lv )
         pW_in_Tube = geom.structure.Placement( 'placeW_in_Tube_'+self.name, volume = wire_lv )
         main_lv.placements.append( pS_in_Tube.name )
         main_lv.placements.append( pW_in_Tube.name )
-
-        self.add_volume(main_lv)
