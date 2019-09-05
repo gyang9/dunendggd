7d853e5258517580adc87c231348e661c3cd8fce
Mike <mike.kordosky@gmail.com>
2019-09-04 23:14:04 -0400

    a small script one can source to build the hall and detectors inside

---
+++ build_hall.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+#
+
+gegede-cli duneggd/Config/WORLDggd.cfg \
+	   duneggd/Config/ND_Hall_Air_Volume.cfg \
+	   duneggd/Config/ND_ElevatorStruct.cfg \
+	   duneggd/Config/KLOE_with_3DST.cfg \
+	   duneggd/Config/MPD_Concept.cfg \
+	   duneggd/Config/ND_CraneRailStruct1.cfg \
+	   duneggd/Config/ND_CraneRailStruct2.cfg \
+	   duneggd/Config/ND_HallwayStruct.cfg \
+	   duneggd/Config/ND_CryoStruct.cfg \
+	   duneggd/Config/ArgonCube/*.cfg \
+	   -w World -o test.gdml
