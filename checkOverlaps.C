57ef704836cd6501f1db3a2fa16920d1e203fedd
Mike Kordosky <mike.kordosky@gmail.com>
2019-09-14 00:36:22 -0400

    Adds a root macro that checks overlaps and draws them.

---
+++ checkOverlaps.C
@@ -0,0 +1,30 @@
+void checkOverlaps(TString filename)
+{
+	TGeoManager *geo = new TGeoManager();
+	geo->Import(filename);
+
+	cout<<"======================== Checking Geometry ============================="<<endl;
+	geo->CheckGeometry();
+	cout<<"========================       Done!       ============================="<<endl;
+
+	
+	cout<<"======================== Checking Overlaps ============================="<<endl;
+	geo->CheckOverlaps(1e-5);
+	geo->PrintOverlaps();
+	cout<<"========================       Done!       =============================\n\n\n"<<endl;
+
+	TObjArray* overlaps=geo->GetListOfOverlaps();
+	for(int i=0; i<overlaps->GetEntries(); i++){
+	  TObject* overlap=overlaps->At(i);
+	  cout<<"========================  Drawing Overlaps ============================="<<endl;
+	  cout<<"================= Overlap messages will duplicate below ================"<<endl;
+	  cout<<"=================     Overlaps are in units of cm       ================"<<endl;
+	  TCanvas* c = new TCanvas();
+	  overlap->Draw("");
+	  TCanvas* cogl = new TCanvas();
+	  overlap->Draw("ogl");
+	  cout<<"========================       Done!       =============================\n\n\n"<<endl;
+	  
+	}
+
+}
