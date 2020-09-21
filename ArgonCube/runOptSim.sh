5c1a15112956497bb3dc3fab08cdb43b1038e92f
PPKoller <patrick_koller@gmx.ch>
2020-09-21 22:23:34 +0200

    Patrick: OptSim.gdml name consistent with ArCubeOptSim.

--- ArgonCube/runOptSim.sh
+++ ArgonCube/runOptSim.sh
@@ -1,2 +1,2 @@
-gegede-cli ../duneggd/Config/ArgonCube/OptSim.cfg ../duneggd/Config/ArgonCube/DETENCLOSURE_OptSim.cfg ../duneggd/Config/WORLDggd.cfg -w World -o optSim.gdml
-root -l 'materialDisplay.C("optSim.gdml")'
+gegede-cli ../duneggd/Config/ArgonCube/OptSim.cfg ../duneggd/Config/ArgonCube/DETENCLOSURE_OptSim.cfg ../duneggd/Config/WORLDggd.cfg -w World -o OptSim.gdml
+root -l 'materialDisplay.C("OptSim.gdml")'
