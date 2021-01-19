eb85be34373c367e6483d3c2e77d2571a5a2590f
PPKoller <patrick_koller@gmx.ch>
2021-01-19 18:37:36 +0100

    Patrick: pixels/copynumbers enabled.

--- ArgonCube/runDetector.sh
+++
@@ -1,2 +0,0 @@
-gegede-cli ../duneggd/Config/ArgonCube/ArgonCube_2x2.cfg ../duneggd/Config/ArgonCube/DETENCLOSURE_Detector.cfg ../duneggd/Config/WORLDggd.cfg -w World -o Detector.gdml
-root -l 'materialDisplay.C("Detector.gdml")'
